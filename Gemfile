d23a81dacca59c4bbf0b25aa062a745e93990f99
Waniazlan <waniazlan27@gmail.com>
2024-09-20 10:15:00 +0800

    new commit

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem "cssbundling-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 gem 'tomo'
+gem "importmap-rails"
+
 gem 'responders', '~> 3.0'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  get "rooms/index"
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
   root 'categories#index'
+  
+  resources :rooms do
+    resources :messages
+  end
+
+  resources :users
   
   resources :categories do
     resources :products
   end
-  
 end
